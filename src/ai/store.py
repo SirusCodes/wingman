e7d3254974a462a9a224dc23cebcf7195a5eb8f4
SirusCodes <hey@darshanrander.com>
2026-02-09 00:28:08 +0530

    feat: setup cloud chroma

--- src/ai/store.py
+++ src/ai/store.py
@@ -1,3 +1,4 @@
+import os
 from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
@@ -6,7 +7,9 @@ def get_vector_store(collection_name: str) -> Chroma:
     embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
     vector_store = Chroma(
         collection_name=collection_name,
-        persist_directory="./chroma_db",
         embedding_function=embeddings,
+        chroma_cloud_api_key=os.getenv("CHROMA_API_KEY"),
+        tenant=os.getenv("CHROMA_TENANT"),
+        database=os.getenv("CHROMA_DATABASE"),
     )
     return vector_store
